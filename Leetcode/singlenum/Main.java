68176ff11070b811e288cc005f13b9f17630de89
Jennifer Wu <jenn.j.wu@gmail.com>
2015-06-07 10:18:08 -0700

    single number problem

---
+++ Leetcode/singlenum/Main.java
@@ -0,0 +1,17 @@
+package Leetcode.singlenum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArr = new int[]{4,5,5,1,4,2,2};
+
+        for (int i = 0; i < intArr.length; i++){
+            System.out.println(intArr[i]);
+        }
+
+
+
+
+
+    }
+}
---
+++ Leetcode/singlenum/Single.java
@@ -0,0 +1,10 @@
+package singlenum;
+
+import java.util.Arrays;
+
+/**
+ * Created by jennjwu on 3/6/15.
+ */
+public class Single {
+
+}
