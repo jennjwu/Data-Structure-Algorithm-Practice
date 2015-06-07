d5a71e32101f11474bb7a16a47f391bb00712c28
Jennifer Wu <jenn.j.wu@gmail.com>
2015-06-07 10:19:05 -0700

    single number problem 2

--- Leetcode/singlenum/Main.java
+++ Leetcode/singlenum/Main.java
@@ -3,15 +3,11 @@ package Leetcode.singlenum;
 public class Main {
 
     public static void main(String[] args) {
-        int[] intArr = new int[]{4,5,5,1,4,2,2};
+        int[] intArr = new int[]{4,5,5,1,1,3,4,2,2};
 
-        for (int i = 0; i < intArr.length; i++){
-            System.out.println(intArr[i]);
-        }
-
-
-
-
+        Single test = new Single();
+        int result = test.singleNumber(intArr);
+        System.out.println(result);
 
     }
 }
--- Leetcode/singlenum/Single.java
+++ Leetcode/singlenum/Single.java
@@ -1,10 +1,24 @@
-package singlenum;
+package Leetcode.singlenum;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
-/**
- * Created by jennjwu on 3/6/15.
- */
 public class Single {
+    public int singleNumber(int[] nums){
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++){
 
+            if (newList.contains(nums[i])){
+                int j = newList.indexOf(nums[i]);
+                newList.remove(j);
+            }
+            else{
+                newList.add(nums[i]);
+            }
+        }
+
+        Integer[] arr = newList.toArray(new Integer[newList.size()]);
+
+        return arr[0];
+
+    }
 }
