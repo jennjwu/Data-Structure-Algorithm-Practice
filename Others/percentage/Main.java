1c89f0819776444ada4ea57bf10ca28222ab41e3
Jennifer Wu <jenn.j.wu@gmail.com>
2015-11-06 22:49:08 -0800

    practice

---
+++ Others/percentage/Main.java
@@ -0,0 +1,49 @@
+package Others.percentage;
+
+import java.util.HashMap;
+
+/**
+ * Created by jennjwu on 11/6/15.
+ */
+public class Main {
+    public static String getItem(HashMap<String, Double> input){
+        Double prob = Math.random()*100;
+        System.out.println(prob);
+
+        Double p = 0.0;
+        for(String key: input.keySet()) {
+            p += input.get(key); //get probability of item
+            if (prob <= p) {
+                return key;
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        HashMap magic = new HashMap();
+        magic.put("Box", 25.0);
+        magic.put("Sword", 25.0);
+        magic.put("Potion", 30.0);
+        magic.put("Knife", 20.0);
+
+        String item = getItem(magic);
+        System.out.println("Your item is " + item);
+
+        HashMap items = new HashMap();
+        items.put("hair dryer", 10.0);
+        items.put("bag", 5.0);
+        items.put("laptop", 15.0);
+        items.put("clothes", 10.0);
+        items.put("air", -5.0);
+        items.put("textbook", 20.0);
+        items.put("shoes", 15.0);
+        items.put("blanket", 30.0);
+
+        String home = getItem(items);
+        System.out.println("You picked: " + home);
+    }
+
+
+
+}
